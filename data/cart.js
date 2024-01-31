1c9913785bfba9f5e4f22f39dfefde11a7ededf2
xiaopingwang <xiaoping.wang1989@gmail.com>
2024-01-31 12:30:47 -0800

    Delete product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {productId:
     "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2}, 
   {productId: 
@@ -23,4 +23,14 @@ export function addToCart(productId, _quantity = 1) {
         quantity: 1
       });
     }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach(item => {
+    if (item.productId !== productId) {
+      newCart.push(item);
+    }
+  })
+  cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,91 +1,105 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { findProductByProductId } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let cartSummaryHTML = '';
-cart.forEach((cartItem) => {
+generateCartSummaryHTML();
 
-  const productId = cartItem.productId;
+function generateCartSummaryHTML() {
+  let cartSummaryHTML = '';
+  console.log('the cart: ');
+  
+  console.log(cart);
+  cart.forEach((cartItem) => {
+
+    const productId = cartItem.productId;
 
 
-  const matchingProduct = findProductByProductId(productId);
+    const matchingProduct = findProductByProductId(productId);
 
-  cartSummaryHTML += `<div class="cart-item-container">
-  <div class="delivery-date">
-    Delivery date: Tuesday, June 21
-  </div>
-
-  <div class="cart-item-details-grid">
-    <img class="product-image"
-      src="${matchingProduct.image}">
-
-    <div class="cart-item-details">
-      <div class="product-name">
-        ${matchingProduct.name}
-      </div>
-      <div class="product-price">
-        $${formatCurrency(matchingProduct.priceCents)}
-      </div>
-      <div class="product-quantity">
-        <span>
-          Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-        </span>
-        <span class="update-quantity-link link-primary">
-          Update
-        </span>
-        <span class="delete-quantity-link link-primary">
-          Delete
-        </span>
-      </div>
+    cartSummaryHTML += `<div class="cart-item-container">
+    <div class="delivery-date">
+      Delivery date: Tuesday, June 21
     </div>
 
-    <div class="delivery-options">
-      <div class="delivery-options-title">
-        Choose a delivery option:
-      </div>
-      <div class="delivery-option">
-        <input type="radio" checked
-          class="delivery-option-input"
-          name="delivery-option-${productId}">
-        <div>
-          <div class="delivery-option-date">
-            Tuesday, June 21
-          </div>
-          <div class="delivery-option-price">
-            FREE Shipping
-          </div>
+    <div class="cart-item-details-grid">
+      <img class="product-image"
+        src="${matchingProduct.image}">
+
+      <div class="cart-item-details">
+        <div class="product-name">
+          ${matchingProduct.name}
+        </div>
+        <div class="product-price">
+          $${formatCurrency(matchingProduct.priceCents)}
+        </div>
+        <div class="product-quantity">
+          <span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+          </span>
+          <span class="js-update-link update-quantity-link link-primary">
+            Update
+          </span>
+          <span data-product-id="${matchingProduct.id}" class="js-delete-link delete-quantity-link link-primary">
+            Delete
+          </span>
         </div>
       </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-${productId}">
-        <div>
-          <div class="delivery-option-date">
-            Wednesday, June 15
-          </div>
-          <div class="delivery-option-price">
-            $4.99 - Shipping
+
+      <div class="delivery-options">
+        <div class="delivery-options-title">
+          Choose a delivery option:
+        </div>
+        <div class="delivery-option">
+          <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-${productId}">
+          <div>
+            <div class="delivery-option-date">
+              Tuesday, June 21
+            </div>
+            <div class="delivery-option-price">
+              FREE Shipping
+            </div>
           </div>
         </div>
-      </div>
-      <div class="delivery-option">
-        <input type="radio"
-          class="delivery-option-input"
-          name="delivery-option-${productId}">
-        <div>
-          <div class="delivery-option-date">
-            Monday, June 13
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${productId}">
+          <div>
+            <div class="delivery-option-date">
+              Wednesday, June 15
+            </div>
+            <div class="delivery-option-price">
+              $4.99 - Shipping
+            </div>
           </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
+        </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-${productId}">
+          <div>
+            <div class="delivery-option-date">
+              Monday, June 13
+            </div>
+            <div class="delivery-option-price">
+              $9.99 - Shipping
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>`;
-});
+  </div>`;
+  });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+  document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+}
+
+document.querySelectorAll('.js-delete-link').forEach(link => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+  })
+})
 
