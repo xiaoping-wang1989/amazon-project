6004e368c0a9cec574efbacc5aa6d1cfc814dcaf
xiaopingwang <xiaoping.wang1989@gmail.com>
2024-02-05 17:09:32 -0800

    correct cartQuantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,8 @@ import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import formatCurrency from './utils/money.js';
 
+updateCartQuantity();
+
 let productsHTML = '';
 
 products.forEach((product, _index) => {
