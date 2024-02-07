261440c2e5b3e504b0f921b477621df38909335a
xiaopingwang <xiaoping.wang1989@gmail.com>
2024-02-07 09:42:47 -0800

    rename tests-jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from "../scripts/utils/money.js";
+import formatCurrency from "../../scripts/utils/money.js";
 
 console.log('test suite: formatCurrency');
 console.log('converts cents into dollars')
